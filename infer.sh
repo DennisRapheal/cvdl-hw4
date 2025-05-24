bdb2ae31bde6823bd84107fac8286e501ed1c5d7
jzwang <a15923647@gmail.com>
2025-05-24 11:55:17 +0000

    train for model-2 infer

--- infer.sh
+++ infer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python ./inference.py \
-  --ckpt ./train_ckpt/model-3/epoch=199-step=18000.ckpt \
+  --ckpt ./train_ckpt/model-2/epoch=199-step=18000.ckpt \
   --input_dir ./hw4_realse_dataset/test/degraded \
-  --output_dir ./outputs/model-3
+  --output_dir ./outputs/model-2
